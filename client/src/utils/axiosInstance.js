8c02be91bd9bff4a8bf167b4e3b9ee6a4260f3bb
roberbaca <roberto.nicolas.baca@gmail.com>
2023-01-09 12:52:10 -0300

    configuracion de railway

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://whos-that-pokemon.onrender.com/api" // mi backend
-    //baseURL: "http://localhost:3100/api"                // mi localhost
+    //baseURL: "https://whos-that-pokemon.onrender.com/api" // mi backend
+    baseURL: "http://localhost:3100/api"                // mi localhost
 });
 
 export default axiosInstance;
