ebe79a8d7493c34adb07e4403b2cd02ce0aad130
roberbaca <roberto.nicolas.baca@gmail.com>
2023-01-09 15:52:25 -0300

    build ready

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://pokegame-production.up.railway.app/api" // mi backend
+    baseURL: "pokegame-production-680b.up.railway.app/api" // mi backend
     //baseURL: "http://localhost:3100/api"                // mi localhost
 });
 
